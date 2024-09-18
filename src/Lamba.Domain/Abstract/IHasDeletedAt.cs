d2b7e3016bcda17cdfb755585d01e476ef746049
alperenkucukali <alperen171195@hotmail.com>
2024-09-18 23:21:45 +0300

    first shot

---
+++ src/Lamba.Domain/Abstract/IHasDeletedAt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Domain.Abstract
+{
+    internal interface IHasDeletedAt
+    {
+        public DateTime? DeletedAt { get; set; }
+    }
+}
