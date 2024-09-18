4e0b3988cb49b1aae4e5ea3dd6250e3b4c3bc3be
alperenkucukali <alperen171195@hotmail.com>
2024-09-18 23:59:13 +0300

    feature/redis

---
+++ src/Lamba.Cache/Abstract/ILambaCacheManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Cache.Abstract
+{
+    public interface ILambaCacheManager
+    {
+        Task<T?> GetAsync<T>(string key, string? childKey = null);
+        Task<bool> SetAsync<T>(string key, string? childKey, T value, TimeSpan? ttl = null);
+        Task<bool> DeleteAsync(string key, string? childKey);
+        Task<bool> UpdateKeyExpireAsync(string key, string? childKey, TimeSpan ttl);
+        Task<TimeSpan?> GetKeyExpireTimeAsync(string key, string? childKey);
+        Task<bool> KeyExistsAsync(string key, string? childKey);
+    }
+}
---
+++ src/Lamba.Cache/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Lamba.Cache.Abstract;
+using Lamba.Cache.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Cache
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddLambaCacheServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ILambaCacheManager, RedisCacheManager>();
+            return services;
+        }
+    }
+}
