ec4c13305043b38ee72569b5a1754a500c3e0e0d
alperenkucukali <alperen171195@hotmail.com>
2024-09-21 00:32:05 +0300

    security layer

---
+++ src/Lamba.Security/Abstract/ITokenProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Lamba.Security.Abstract
+{
+    public interface ITokenProvider
+    {
+        string CreateToken(IEnumerable<Claim>? claims = null);
+    }
+}
---
+++ src/Lamba.Security/Common/HashHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Lamba.Security.Common
+{
+    public static class HashHelper
+    {
+        public static string ComputeHash(string password, string salt)
+        {
+            using var sha256 = SHA256.Create();
+            var byteVal = Encoding.UTF8.GetBytes(password + salt);
+            var byteHash = sha256.ComputeHash(byteVal);
+            return Convert.ToBase64String(byteHash);
+        }
+
+        public static string GenerateSalt(uint size = 64)
+        {
+            using var rng = RandomNumberGenerator.Create();
+            var byteSalt = new byte[size];
+            rng.GetBytes(byteSalt);
+            return Convert.ToBase64String(byteSalt);
+        }
+    }
+}
