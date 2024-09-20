67e683c563469a7ce76f6983410de30ebc1da8f8
alperenkucukali <alperen171195@hotmail.com>
2024-09-20 23:29:12 +0300

    http client

---
+++ src/Lamba.HttpClient/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Lamba.HttpClient.Abstract;
+using Lamba.HttpClient.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lamba.HttpClient
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddLambaHttpClientServices(this IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddSingleton<ILambaHttpClient, LambaHttpClient>();
+            return services;
+        }
+    }
+}
