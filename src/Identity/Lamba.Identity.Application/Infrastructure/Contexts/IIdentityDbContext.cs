294b3566db9ce2e0d17932b7550b6ce26a605ec6
alperenkucukali <alperen171195@hotmail.com>
2024-09-26 23:01:49 +0300

    identity application layer

---
+++ src/Identity/Lamba.Identity.Application/Infrastructure/Contexts/IIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lamba.Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Identity.Application.Infrastructure.Contexts
+{
+    public interface IIdentityDbContext
+    {
+        DbSet<User> Users { get; }
+    }
+}
---
+++ src/Identity/Lamba.Identity.Application/Infrastructure/Contexts/IIdentityWriterDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Identity.Application.Infrastructure.Contexts
+{
+    public interface IIdentityWriterDbContext : IIdentityDbContext
+    {
+    }
+}
