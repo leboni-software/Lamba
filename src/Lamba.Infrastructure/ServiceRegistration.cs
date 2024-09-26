620406593652db299fd4e1dd8752fec3078ffccf
alperenkucukali <alperen171195@hotmail.com>
2024-09-26 22:32:06 +0300

    infrastructure service registration

--- src/Lamba.Infrastructure/ServiceRegistration.cs
+++ src/Lamba.Infrastructure/ServiceRegistration.cs
@@ -12,23 +12,22 @@ namespace Lamba.Infrastructure
 {
     public static class ServiceRegistration
     {
-        public static IServiceCollection AddLambaInfrastructureServices(this IServiceCollection services, IConfiguration configuration, bool isDebug)
+        public static void AddLambaInfrastructureServices(this IServiceCollection services, IConfiguration configuration, bool isDevelopmentEnvironment)
         {
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);            
             services.AddDbContext<BaseWriterDbContext>(opt =>
             {
-                opt.EnableDetailedErrors(isDebug);
-                opt.EnableSensitiveDataLogging(isDebug);
+                opt.EnableDetailedErrors(isDevelopmentEnvironment);
+                opt.EnableSensitiveDataLogging(isDevelopmentEnvironment);
                 opt.UseNpgsql(configuration.GetConnectionString("WriterConnectionString"), sql => sql.EnableRetryOnFailure(3));
             });
             services.AddDbContext<BaseReaderDbContext>(opt =>
             {
-                opt.EnableDetailedErrors(isDebug);
-                opt.EnableSensitiveDataLogging(isDebug);
+                opt.EnableDetailedErrors(isDevelopmentEnvironment);
+                opt.EnableSensitiveDataLogging(isDevelopmentEnvironment);
                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 opt.UseNpgsql(configuration.GetConnectionString("ReaderConnectionString"), sql => sql.EnableRetryOnFailure(3));
             });
-            return services;
         }
     }
 }
