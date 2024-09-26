4aaf612eedd1b97d37616bdd5dd6be869f39f925
alperenkucukali <alperen171195@hotmail.com>
2024-09-26 23:01:40 +0300

    identity domain layer

---
+++ src/Identity/Lamba.Identity.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Lamba.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Identity.Domain.Entities
+{
+    public class User : BaseAuditEntity<Guid>
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string PasswordSalt { get; set; }
+    }
+}
