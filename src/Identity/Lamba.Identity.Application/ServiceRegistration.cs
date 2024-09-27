616a71d5f4ce82b74894c74ded4d700a3f4c6e19
alperenkucukali <alperen171195@hotmail.com>
2024-09-27 22:48:27 +0300

    application layer service registration

---
+++ src/Identity/Lamba.Identity.Application/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Lamba.Security;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Lamba.Identity.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddLambaIdentityApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddLambaSecurityServices(configuration);
+            services.AddMediatR(conf =>
+            {
+                conf.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            });
+        }
+    }
+}
