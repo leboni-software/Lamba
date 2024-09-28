7f7e7fc9b16c59ce5c39985aff3489596eec2ab3
alperenkucukali <alperen171195@hotmail.com>
2024-09-29 00:22:10 +0300

    authentication command

---
+++ src/Identity/Lamba.Identity.Application/Features/Commands/Authentications/RegisterCommand.cs
@@ -0,0 +1,60 @@
+﻿using Lamba.Identity.Application.Features.Commands.Authentications.Dto;
+using Lamba.Identity.Application.Infrastructure.Repositories.Readers;
+using Lamba.Identity.Application.Infrastructure.Repositories.Writers;
+using Lamba.Identity.Domain.Entities;
+using Lamba.Repository.Abstract;
+using Lamba.Security.Abstract;
+using Lamba.Security.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Identity.Application.Features.Commands.Authentications
+{
+    public record RegisterCommand : IRequest<RegisterReponseDto>
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisterReponseDto>
+    {
+        private readonly ITokenProvider _tokenProvider;
+        private readonly IUserWriterRepository _userWriterRepository;
+        private readonly IRoleReaderRepository _roleReaderRepository;
+
+        public RegisterCommandHandler(ITokenProvider tokenProvider, IUserWriterRepository userWriterRepository, IRoleReaderRepository roleReaderRepository)
+        {
+            _tokenProvider = tokenProvider;
+            _userWriterRepository = userWriterRepository;
+            _roleReaderRepository = roleReaderRepository;
+        }
+
+        public async Task<RegisterReponseDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            var passwordSalt = HashHelper.GenerateSalt();
+            var user = new User
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Username = request.Username,
+                Email = request.Email,
+                PasswordSalt = passwordSalt,
+                Password = HashHelper.ComputeHash(request.Password, passwordSalt)
+            };
+            var defaultRole = await _roleReaderRepository.GetAsync(x => x.IsDefaultRole, cancellationToken);
+            if (defaultRole is null) throw new Exception("The default role was not found!");
+            throw new NotImplementedException();
+            //await _userWriterRepository.AddAsync(user, cancellationToken);
+            //await _userWriterRepository.SaveChangesAsync(cancellationToken);
+            //var token = _tokenProvider.CreateToken();
+            //return new RegisterReponseDto { Token = token };
+        }
+    }
+}
