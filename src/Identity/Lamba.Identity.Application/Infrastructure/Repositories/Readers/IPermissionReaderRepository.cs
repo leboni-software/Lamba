caba3c9787f29c10901b185941b7602d54875199
alperenkucukali <alperen171195@hotmail.com>
2024-10-16 22:59:59 +0300

    permission repositories

---
+++ src/Identity/Lamba.Identity.Application/Infrastructure/Repositories/Readers/IPermissionReaderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lamba.Identity.Domain.Entities;
+using Lamba.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Identity.Application.Infrastructure.Repositories.Readers
+{
+    public interface IPermissionReaderRepository : IReaderRepository<Permission, Guid>
+    {
+    }
+}
---
+++ src/Identity/Lamba.Identity.Infrastructure/Data/Repositories/Writers/PermissionWriterRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lamba.Identity.Application.Infrastructure.Repositories.Writers;
+using Lamba.Identity.Domain.Entities;
+using Lamba.Identity.Infrastructure.Data.Contexts;
+using Lamba.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Identity.Infrastructure.Data.Repositories.Writers
+{
+    public class PermissionWriterRepository(IdentityWriterDbContext dbContext) : BaseWriterRepository<Permission, Guid, IdentityWriterDbContext>(dbContext), IPermissionWriterRepository
+    {
+    }
+}
