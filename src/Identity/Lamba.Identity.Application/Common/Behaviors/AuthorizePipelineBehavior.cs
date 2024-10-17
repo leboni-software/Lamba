9fa9e9c1f15c49ce1a51457072fd0d22b2023eef
alperenkucukali <alperen171195@hotmail.com>
2024-10-17 23:09:47 +0300

    authorize pipeline edits

--- src/Identity/Lamba.Identity.Application/Common/Behaviors/AuthorizePipelineBehavior.cs
+++ src/Identity/Lamba.Identity.Application/Common/Behaviors/AuthorizePipelineBehavior.cs
@@ -27,24 +27,23 @@ namespace Lamba.Identity.Application.Common.Behaviours
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
+            var requestName = typeof(TRequest).Name;
             try
             {
-                var requestName = typeof(TRequest).Name;
                 var roles = _contextAccessor.HttpContext!.User.Claims
                     .Where(x => x.Type == LambaSecurityConstants.RoleClaim)
                     .Select(x => x.Value.Split(','))
-                    .First();
+                    .FirstOrDefault() ?? throw new Exception("The user does not have any role!");
                 var hasPermission = await _roleReaderRepository.GetQueryable()
                     .Where(x => roles.Contains(x.Name))
                     .Where(x => x.PermissionRoles.Any(perm => perm.Permission.CommandName == requestName))
                     .AnyAsync(cancellationToken);
                 if (!hasPermission) throw new UnauthorizedAccessException();
-
                 return await next();
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, $"Authorize Error Occured! {typeof(TRequest)} {request}");
+                _logger.LogError(ex, $"Unauthorized access detected! {requestName} {request}");
                 throw;
             }
         }
