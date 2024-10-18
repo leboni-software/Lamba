7c2f8f51094aca73f2aefb8525c6a3a81de82df2
alperenkucukali <alperen171195@hotmail.com>
2024-10-18 23:47:05 +0300

    results

---
+++ src/Lamba.Common/Models/Results/ErrorResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Common.Models.Results
+{
+    public sealed record ErrorResult(string Message) : Result(false, Message);
+    public sealed record ErrorResult<T>(T? Data, string Message) : Result<T>(Data, false, Message);
+}
---
+++ src/Lamba.Common/Models/Results/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Common.Models.Results
+{
+    public record Result(bool IsSuccess, string Message);
+    public record Result<T>(T? Data, bool IsSuccess, string Message) : Result(IsSuccess, Message);
+}
