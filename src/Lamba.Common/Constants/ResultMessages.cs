8164fcf921521c268ef9ca4503c0863dc1e6adba
alperenkucukali <alperen171195@hotmail.com>
2024-10-19 23:10:46 +0300

    common layer

---
+++ src/Lamba.Common/Constants/ResultMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba.Common.Constants
+{
+    public static class ResultMessages
+    {
+        public const string SuccessfulOperation = "Operation completed successfully!";
+        public const string FailedOperation = "Operation completed with error!";
+    }
+}
