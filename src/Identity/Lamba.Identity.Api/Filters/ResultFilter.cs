2498956a29f7a1b8c0ff9a8deaed4cf6753e7736
alperenkucukali <alperen171195@hotmail.com>
2024-10-19 23:00:21 +0300

    result filter added

---
+++ src/Identity/Lamba.Identity.Api/Filters/ResultFilter.cs
@@ -0,0 +1,21 @@
+﻿using Lamba.Common.Models.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lamba.Identity.Api.Filters
+{
+    public class ResultFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var executedContext = await next();
+            if (executedContext.Result is ObjectResult objectResult && objectResult.Value is not Result)
+            {
+                if (objectResult.Value is null)
+                    executedContext.Result = new OkObjectResult(new SuccessResult("Operation completed successfully!"));
+                else
+                    executedContext.Result = new OkObjectResult(new SuccessResult<object>(objectResult.Value, "Operation completed successfully!"));
+            }
+        }
+    }
+}
