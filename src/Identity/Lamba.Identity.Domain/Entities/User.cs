c75aac2c26aad3513bc609d50867b378910f08b0
alperenkucukali <alperen171195@hotmail.com>
2024-10-21 23:37:02 +0300

    encapsulate user entity

--- src/Identity/Lamba.Identity.Domain/Entities/User.cs
+++ src/Identity/Lamba.Identity.Domain/Entities/User.cs
@@ -1,15 +1,52 @@
 ﻿using Lamba.Domain.Concrete;
+using Lamba.Security.Common;
 
 namespace Lamba.Identity.Domain.Entities
 {
     public class User : BaseAuditEntity<Guid>
     {
-        public virtual required string FirstName { get; set; }
-        public virtual required string LastName { get; set; }
-        public virtual required string Username { get; set; }
-        public virtual required string Email { get; set; }
-        public virtual required string Password { get; set; }
-        public virtual required string PasswordSalt { get; set; }
+        public virtual string FirstName { get; private set; } = null!;
+        public virtual string LastName { get; private set; } = null!;
+        public virtual string Username { get; private set; } = null!;
+        public virtual string Email { get; private set; } = null!;
+        public virtual string Password { get; private set; } = null!;
+        public virtual string PasswordSalt { get; private set; } = null!;
         public virtual ICollection<UserRole> UserRoles { get; set; } = [];
+
+        public User()
+        {
+
+        }
+
+        public User(string firstName, string lastName, string username, string email, string password)
+        {
+            SetFirstName(firstName);
+            SetLastName(lastName);
+            SetUsername(username);
+            SetEmail(email);
+            SetPassword(password);
+        }
+
+        public void SetFirstName(string firstName)
+        {
+            FirstName = firstName;
+        }
+        public void SetLastName(string lastName)
+        {
+            LastName = lastName;
+        }
+        public void SetUsername(string username)
+        {
+            Username = username;
+        }
+        public void SetEmail(string email)
+        {
+            Email = email;
+        }
+        public void SetPassword(string password)
+        {
+            PasswordSalt = HashHelper.GenerateSalt();
+            Password = HashHelper.ComputeHash(password, PasswordSalt);
+        }
     }
 }
