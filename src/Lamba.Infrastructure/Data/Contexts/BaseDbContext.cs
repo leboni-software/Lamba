1427a61a19d471c8b79d1065c2e4ecaa9fbcb969
alperenkucukali <alperen171195@hotmail.com>
2024-10-22 22:16:27 +0300

    overrride method moved to basedbcontext

--- src/Lamba.Infrastructure/Data/Contexts/BaseDbContext.cs
+++ src/Lamba.Infrastructure/Data/Contexts/BaseDbContext.cs
@@ -1,10 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Lamba.Domain.Abstract;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lamba.Infrastructure.Data.Contexts
 {
     public class BaseDbContext<T>(DbContextOptions<T> options) : DbContext(options)
         where T : DbContext
     {
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                 .Entries()
+                 .Where(x => x.Entity is IHasUpdatedAt or IHasDeletedAt)
+                 .Where(x => x.State == EntityState.Modified || x.State == EntityState.Deleted);
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Modified && entry.Entity is IHasUpdatedAt)
+                {
+                    ((IHasUpdatedAt)entry.Entity).UpdatedAt = DateTime.UtcNow;
+                }
+                else if (entry.State == EntityState.Deleted && entry.Entity is IHasDeletedAt)
+                {
+                    entry.State = EntityState.Modified;
+                    ((IHasDeletedAt)entry.Entity).DeletedAt = DateTime.UtcNow;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(BaseDbContext<T>).Assembly);
--- src/Lamba.Infrastructure/Data/Contexts/BaseWriterDbContext.cs
+++ src/Lamba.Infrastructure/Data/Contexts/BaseWriterDbContext.cs
@@ -7,26 +7,5 @@ namespace Lamba.Infrastructure.Data.Contexts
     public class BaseWriterDbContext<TContext>(DbContextOptions<TContext> options) : BaseDbContext<TContext>(options)
         where TContext : BaseDbContext<TContext>
     {
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            var entries = ChangeTracker
-                 .Entries()
-                 .Where(x => x.Entity is IHasUpdatedAt or IHasDeletedAt)
-                 .Where(x => x.State == EntityState.Modified || x.State == EntityState.Deleted);
-
-            foreach (var entry in entries)
-            {
-                if (entry.State == EntityState.Modified && entry.Entity is IHasUpdatedAt)
-                {
-                    ((IHasUpdatedAt)entry.Entity).UpdatedAt = DateTime.UtcNow;
-                }
-                else if (entry.State == EntityState.Deleted && entry.Entity is IHasDeletedAt)
-                {
-                    entry.State = EntityState.Modified;
-                    ((IHasDeletedAt)entry.Entity).DeletedAt = DateTime.UtcNow;
-                }
-            }
-            return base.SaveChangesAsync(cancellationToken);
-        }
     }
 }
