3005063202273cc2e66abc9c3d84e003a12ec153
alperenkucukali <alperen171195@hotmail.com>
2024-10-24 22:55:34 +0300

    lamba logger added

---
+++ src/Lamba.Logger/ServiceRegistration.cs
@@ -0,0 +1,44 @@
+﻿using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+
+namespace Lamba.Logger
+{
+    public static class ServiceRegistration
+    {
+        public static void UseLambaLogger(this ConfigureHostBuilder host, string dataset)
+        {
+            host.UseSerilog((context, configuration) =>
+            {
+                configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithEnvironmentName()
+                .WriteTo.Elasticsearch([new Uri(context.Configuration["ElasticConfigurations:Uri"]!)],
+                opt =>
+                {
+                    opt.DataStream = new DataStreamName("logs", dataset, context.HostingEnvironment.EnvironmentName);
+                    opt.BootstrapMethod = BootstrapMethod.Failure;
+                },
+                transport =>
+                {
+                    transport.Authentication(
+                        new BasicAuthentication(
+                            context.Configuration["ElasticConfigurations:Username"]!,
+                            context.Configuration["ElasticConfigurations:Password"]!)
+                        );
+                }, restrictedToMinimumLevel: LogEventLevel.Error);
+                if (context.HostingEnvironment.IsDevelopment() || context.HostingEnvironment.IsEnvironment("Local"))
+                    configuration.WriteTo.Console(LogEventLevel.Information);
+            });
+        }
+    }
+}
