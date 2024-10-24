01fc1b01413228ed622460356e44ca74abf17620
alperenkucukali <alperen171195@hotmail.com>
2024-10-25 00:24:39 +0300

    added polly

--- src/Lamba.Logger/ServiceRegistration.cs
+++ src/Lamba.Logger/ServiceRegistration.cs
@@ -5,6 +5,7 @@ using Elastic.Transport;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Polly;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
@@ -18,26 +19,45 @@ namespace Lamba.Logger
             host.UseSerilog((context, configuration) =>
             {
                 configuration
-                .Enrich.FromLogContext()
-                .Enrich.WithExceptionDetails()
-                .Enrich.WithMachineName()
-                .Enrich.WithEnvironmentName()
-                .WriteTo.Elasticsearch([new Uri(context.Configuration["ElasticConfigurations:Uri"]!)],
-                opt =>
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithEnvironmentName();
+
+                var elasticUri = context.Configuration["ElasticConfigurations:Uri"]!;
+                var username = context.Configuration["ElasticConfigurations:Username"]!;
+                var password = context.Configuration["ElasticConfigurations:Password"]!;
+
+                var policy = Policy
+                    .Handle<Exception>()
+                    .WaitAndRetry(
+                        retryCount: 5,
+                        sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
+                        onRetry: (exception, timeSpan, retryCount, context) =>
+                        {
+                            Console.WriteLine($"Error: {exception.Message}");
+                            Console.WriteLine($"Retrying Elasticsearch connection: Attempt {retryCount}. Waiting {timeSpan.TotalSeconds} seconds.");
+                        });
+
+                policy.Execute(() =>
                 {
-                    opt.DataStream = new DataStreamName("logs", dataset, context.HostingEnvironment.EnvironmentName);
-                    opt.BootstrapMethod = BootstrapMethod.Failure;
-                },
-                transport =>
-                {
-                    transport.Authentication(
-                        new BasicAuthentication(
-                            context.Configuration["ElasticConfigurations:Username"]!,
-                            context.Configuration["ElasticConfigurations:Password"]!)
-                        );
-                }, restrictedToMinimumLevel: LogEventLevel.Error);
+                    configuration.WriteTo.Elasticsearch(
+                        [new Uri(elasticUri)],
+                        opt =>
+                        {
+                            opt.DataStream = new DataStreamName("logs", dataset, context.HostingEnvironment.EnvironmentName);
+                            opt.BootstrapMethod = BootstrapMethod.Failure;
+                        },
+                        transport =>
+                        {
+                            transport.Authentication(new BasicAuthentication(username, password));
+                        }, restrictedToMinimumLevel: LogEventLevel.Error);
+                });
+                
                 if (context.HostingEnvironment.IsDevelopment() || context.HostingEnvironment.IsEnvironment("Local"))
+                {
                     configuration.WriteTo.Console(LogEventLevel.Information);
+                }
             });
         }
     }
